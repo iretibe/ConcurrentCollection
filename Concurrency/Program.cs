6a412abd63e3b29cf4081e7a423546cbdf618364
Somad <iretibe@users.noreply.github.com>
2021-09-23 17:14:42 +0000

    Implemented Concurrent Collection in c#

---
+++ Concurrency/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Concurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ordersQueue = new Queue<string>();
+            PlaceOrders(ordersQueue, "John", 6);
+            PlaceOrders(ordersQueue, "Eric", 6);
+
+            foreach (string order in ordersQueue)
+                Console.WriteLine("ORDER: " + order);
+        }
+
+        static void PlaceOrders(Queue<string> orders, string customerName, int nOrders)
+        {
+            for (int i = 1; i <= nOrders; i++)
+            {
+                Thread.Sleep(1);
+                string orderName = $"{customerName} wants t-shirt {i}";
+                orders.Enqueue(orderName);
+            }
+        }
+    }
+}
